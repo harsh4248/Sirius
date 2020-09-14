f78ba53d103933408631f15f7bb4e8b283123aac
Harsh dalwadi <harshdalwadi500@gmail.com>
2020-09-14 21:20:11 +0530

    loginActivity design commit

--- app/src/main/java/com/example/sirius/MainActivity.java
+++ app/src/main/java/com/example/sirius/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,7 +16,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+
 
         myLogo = findViewById(R.id.imageViewmyLogo);
 
