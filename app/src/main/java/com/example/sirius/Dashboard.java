b2fb13e5fcf15677ad3e65db83cfc4b888c7a9e5
Harsh dalwadi <harshdalwadi500@gmail.com>
2020-09-18 02:14:22 +0530

    signup usercreation and design complete

---
+++ app/src/main/java/com/example/sirius/Dashboard.java
@@ -0,0 +1,35 @@
+package com.example.sirius;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Dashboard extends AppCompatActivity {
+
+    Button signout;
+    FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        signout = findViewById(R.id.buttonsignout);
+
+        firebaseAuth=FirebaseAuth.getInstance();
+
+        signout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(Dashboard.this,loginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/sirius/User.java
@@ -0,0 +1,17 @@
+package com.example.sirius;
+
+public class User {
+
+    public String Email,Username;
+
+    public User() {
+
+    }
+
+    public User(String email, String username) {
+        Email = email;
+        Username = username;
+    }
+
+
+}
